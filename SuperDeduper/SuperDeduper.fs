cfdfaccfe563dd6cc54459975e41d8335e9494de
Mark Fields <markfields-git@outlook.com>
2018-07-28 18:06:56 -0700

    Add HashMap class

--- SuperDeduper/SuperDeduper.fs
+++ SuperDeduper/SuperDeduper.fs
@@ -14,13 +14,35 @@ module Main =
     | [DirInfo info] -> Result.Ok info
     | _ -> Result.Error "Couldn't get info about that path"
 
+  type HashMap() =
+    let dict = new System.Collections.Generic.Dictionary<string, string list>()
 
+    // Adds the item and returns the hash for quick lookup
+    member this.AddItem (item: string) =
+      let hash = item.Length.ToString()  //** Pretend hash function
+      let items = 
+        if dict.ContainsKey hash
+        then dict.[hash]
+        else []
+      dict.[hash] <- item::items
+      hash
+    
+    member this.GetItems (hash: string) =
+      if dict.ContainsKey hash
+      then Some dict.[hash]
+      else None
+
+
+  let rec traverse (root: DirectoryInfo) =
+    let children = root.GetFileSystemInfos()
+    ()
 
   [<EntryPoint>]
   let main argv =
     printfn "%A" argv
     result {
       let! (rootInfo) = argv |> parseCommandArgs
+
       return ()
     } |> ignore
     0 // return an integer exit code
